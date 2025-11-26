c11e88aa70dbf82fbeb3fc44dc481ea968e79e03
Musawenkosi <launchpaddigital1@gmail.com>
2025-11-26 17:57:58 +0200

    Fix: adjust relative imports for product detail client

--- app/product/ProductDetailClient.tsx
+++ app/product/ProductDetailClient.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from 'next/image'
 import { useState } from 'react'
-import { useCart } from '../../contexts/CartContext'
+import { useCart } from '../contexts/CartContext'
 import type { Product } from '../../utils/productData'
 
 interface Props {
